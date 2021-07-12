e4a7838b4e9a354ba5031059381162b86430c4b2
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2021-07-12 16:59:40 +0530

    Update linear_search.py

--- Searching/Linear Search/linear_search.py
+++ Searching/Linear Search/linear_search.py
@@ -4,13 +4,13 @@
 #In Linear Search, we start from the 0th index of the array, we compare each element with the key (desired element).
 #If the key is same as the element of array ,we return the index of that element.
 #If the key does not match any element then ,we return -1.
-def search(arr, x): 
-    for i in range(len(arr)): 
-        if arr[i] == x: 
+def search(arr, x): #function to  search.
+    for i in range(len(arr)): #traverses the whole array.
+        if arr[i] == x: #if element is found.
             return i 
-    return -1
+    return -1  #if element is not found.
     
-    
+ #user input
 n=int(input("Enter Size of array: "))
 arr=[]
 print("Enter array elements: ")
@@ -18,7 +18,7 @@ for i in range(n):
   e=int(input())
   arr.append(e)
 x=int(input("Enter element to be searched: "))
-ans=search(arr,x)
+ans=search(arr,x) #function call
 if ans==-1:
   print("Element not found ")
 else:
