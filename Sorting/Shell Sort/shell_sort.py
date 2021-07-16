5ae447e535b0925cb88f2e6cf4fdaee32d6adf41
Thejaswin <52855622+thejaswin123@users.noreply.github.com>
2021-07-16 18:29:30 +0530

    Update shell_sort.py

--- Sorting/Shell Sort/shell_sort.py
+++ Sorting/Shell Sort/shell_sort.py
@@ -1,3 +1,23 @@
+''' This program illustrates the shell sort implementation in Python
+
+ Shell sort or Shell's method, is an in-place comparison sort.
+ It can be seen as either a generalization of sorting by exchange (bubble sort) or sorting by
+ insertion (insertion sort). The method starts by sorting pairs of elements far apart from each other,
+ then progressively reducing the gap between elements to be compared. Starting with far apart elements
+ can move some out-of-place elements into position faster than a simple nearest neighbor exchange.
+
+  Best Case O(n logn); Average Case O(depends on gap sequence); Worst Case O(n^2)'''
+
+# sample Test case    
+""" >>> shell_sort([0, 5, 3, 2, 2])
+    [0, 2, 2, 3, 5]
+    >>> shell_sort([])
+    []
+    >>> shell_sort([-2, -5, -45])
+    [-45, -5, -2]
+    """
+
+
 def shell_sort(arr):
     sublistCount = int(len(arr)/2)
 
