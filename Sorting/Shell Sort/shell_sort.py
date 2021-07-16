2aea7b156703a4f92f395c7f8231c5a5d05d48de
Thejaswin <52855622+thejaswin123@users.noreply.github.com>
2021-07-16 18:34:56 +0530

    Update shell_sort.py

--- Sorting/Shell Sort/shell_sort.py
+++ Sorting/Shell Sort/shell_sort.py
@@ -8,18 +8,10 @@
 
   Best Case O(n logn); Average Case O(depends on gap sequence); Worst Case O(n^2)'''
 
-# sample Test case    
-""" >>> shell_sort([0, 5, 3, 2, 2])
-    [0, 2, 2, 3, 5]
-    >>> shell_sort([])
-    []
-    >>> shell_sort([-2, -5, -45])
-    [-45, -5, -2]
-    """
 
 
-def shell_sort(arr):
-    sublistCount = int(len(arr)/2)
+def shell_sort(arr):                          #function for shell sort
+    sublistCount = int(len(arr)/2)            
 
     while sublistCount > 0:
         for start in range(sublistCount):
@@ -32,11 +24,21 @@ def do_insertsort_gap(arr,start,gap):
         currentValue = arr[i]
         position = i
         while position >= gap and arr[position-gap] >= currentValue:
-            arr[position] = arr[position-gap]
+            arr[position] = arr[position-gap]                             # We compare values in each sub-list and swap them (if necessary) in the original array.
             position = position-gap
         arr[position] = currentValue        
 
-array = input("Enter the elements of the array separated by space : ")
-array = [int(x) for x in array.split(" ")]
+array = input("Enter the elements of the array separated by space : ")    #Taking input
+array = [int(x) for x in array.split(" ")]                                #string to int conversion
 
-print(f"Array after sorting is : {shell_sort(array)}")      
+print(f"Array after sorting is : {shell_sort(array)}")                    #printing the output
+
+
+# sample Test case    
+""" >>> shell_sort([0, 5, 3, 2, 2])
+    [0, 2, 2, 3, 5]
+    >>> shell_sort([])
+    []
+    >>> shell_sort([-2, -5, -45])
+    [-45, -5, -2]
+    """
