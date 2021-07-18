ca7d62532a6bf5244d40cf04dca5cbcd4564d34d
RC2208 <70155541+RC2208@users.noreply.github.com>
2021-07-18 21:01:41 +0530

    Update insertion_sort.py

--- Sorting/Insertion Sort/insertion_sort.py
+++ Sorting/Insertion Sort/insertion_sort.py
@@ -1,17 +1,18 @@
+# Sorting an array of numbers using Insertion sort and counting no. of swaps and comparisons made suring the process
 a=[66, 45, 34, 6]
 swaps=0
 comp=0
 for i in range(1,len(a)):
-    val=a[i]
+    val=a[i]  #assigning it to variable val
     j=i-1 # left elememt to val
     while j >= 0 and val< a[j] :        
         a[j+1]=a[j]  # right element getting val of left
         j=j-1
-        swaps=swaps+1
+        swaps=swaps+1  #increments swaps var
         comp=comp +1
 
-    a[j+1]=val
-    if (val>a[j]):
+    a[j+1]=val   #replaces the swapped number into its sorted location
+    if (val>a[j]):  #increments comparisions for case when no swaps are made
         comp=comp+1
     
 print('No. of swaps= %d'%swaps)
