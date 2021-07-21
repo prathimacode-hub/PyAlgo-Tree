636071d82fee118eeed4b245fc45f10b158d9e17
SANJAY N T <sanjaysanju12120@gmail.com>
2021-07-21 15:51:03 +0530

    Deleting Element of Array

---
+++ Arrays/Accessing_Element.py
@@ -0,0 +1,19 @@
+# accessing of element from list
+
+# importing array
+import array as arr
+
+# array with integer
+a = arr.array('i', [1, 2, 3, 4, 5, 6])
+
+# accessing element with indices
+print("Access First element in array is : ", a[0])
+
+print("Access 3rd element: ", a[3])
+
+# array with floating point
+a = arr.array('d', [1.0, 2.2, 3.3, 4.4])
+
+# accessing element of array
+print("Access 1st element : ", a[0])
+
