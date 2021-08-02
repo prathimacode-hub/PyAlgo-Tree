dea32ddd7b62abb52271135c5c208e8e7f6d85aa
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2021-08-02 21:17:55 +0530

    Update first_index_of_array.py

--- Recursion/First Index/first_index_of_array.py
+++ Recursion/First Index/first_index_of_array.py
@@ -1,16 +1,16 @@
-
+#To find first index of an element in an array.
 def firstIndex(arr, si, x):
-    l = len(arr)
-    if l == 0:
+    l = len(arr) #length of array.
+    if l == 0:  #base case 
         return -1 
-    if arr[si] == x:
+    if arr[si] == x: #if element is found at start index of an array then return that index.
         return si
-    return firstIndex(arr, si+1, x)
+    return firstIndex(arr, si+1, x) #recursive call.
 
-arr = []
+arr = [] #initialised array.
 n=int(input("Enter size of array : "))
-for i in range(n):
+for i in range(n): #input array.
   ele=int(input())
   arr.append(ele)
-x=int(input("Enter element to be searched "))
+x=int(input("Enter element to be searched ")) #element to be searched
 print(firstIndex(arr, 0, x))
